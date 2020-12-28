d7d6ec991ef214b465fcee0b48bd82a8f416ae18
SasikalaGV <49078628+SasikalaGV@users.noreply.github.com>
2020-12-28 01:25:50 +0000

    Changes to DBConfig

--- dbconfigtemplate.py
+++ dbconfigtemplate.py
@@ -1,6 +1,6 @@
 mysql={
-    'host':"localhost",
-    'username':'root',
-    'password':'',
+    'host':"SasikalaGV.mysql.pythonanywhere-services.com",
+    'username':'SasikalaGV',
+    'password':'DataRep2020',
     'database': 'datarepresentation'
 }
